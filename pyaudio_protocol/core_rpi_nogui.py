4818291e544291165560e8824fc389794b487b77
Lx37 <alexandra.corneyllie@cnrs.fr>
2022-09-08 16:08:19 +0200

    remove some prints
    Signed-off-by: Lx37 <alexandra.corneyllie@cnrs.fr>

--- pyaudio_protocol/core_rpi_nogui.py
+++ pyaudio_protocol/core_rpi_nogui.py
@@ -1,30 +1,19 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2016, French National Center for Scientific Research (CNRS)
-# Distributed under the (new) BSD License. See LICENSE for more info.
-
 import os
 import soundfile as sf
 import sounddevice as sd
 import time
+#import datetime
+from time import perf_counter
 
 import RPi.GPIO as GPIO
 import numpy as np
 from threading import Thread, Lock
 from subprocess import call
 
-'''
-This code is the one used on the Raspberry Pi box (no gui)
-
-'''
-
-
-
-import logging
-logger = logging.getLogger()
-
 
 def get_GPIO_bool(trig_value, parralel_GPIO):
     bool_filter = np.array(np.array(list('{0:08b}'.format(trig_value))), dtype=bool)
+    #print('bool_filter : ', bool_filter)
     GPIO_trigOn = parralel_GPIO[bool_filter].tolist()
     return GPIO_trigOn
 
@@ -52,6 +41,17 @@ class sound_trig_Thread(Thread):
     def running(self):
         with self.lock:
             return self._running
+    
+    # Accurate wait for target time (using time.sleep + busy loop for last millisecond)
+    def wait(self, targetDate, txt=''):
+        while True:
+            delta = targetDate - perf_counter()
+            #print('delta : ', delta)
+            if delta <= 0:
+                return True
+            time.sleep( 0.0001*(delta > 0.11) + 0.000002*(delta > 0.003) )
+            #if not self.playing():
+            #    return False
 
     def run(self):
         with self.lock:
@@ -63,10 +63,15 @@ class sound_trig_Thread(Thread):
         Led2 = 0
         Led3 = 0
 
+        startTime = perf_counter()
+        stopTime = perf_counter()
+        endTime = perf_counter()
+
         for index, row in self.playframe.iterrows():   #playframe.iloc[self.current:]
+
             if not self.running():
                 self.current = index
-                logger.info('Stopped at index ' + str(self.current))
+                print('Stopped at index ' + str(self.current))
                 break
 
             if index > round(nb_items/4) and Led1 == 0:
@@ -79,29 +84,49 @@ class sound_trig_Thread(Thread):
                 GPIO.output(self.LEDState_GPIO[3], GPIO.HIGH)
                 Led3 = 1
 
-            logger.debug('index : ', index)
+            # Get next sound and trig
             sound_data, sample_rate = sf.read(self.stim_folder + row['Stimulus'] + '.wav')
-            sound_data = sound_data.astype(self.sound_dtype)
+            sound_data = sound_data.astype(self.sound_dtype) #TODO why sounds are in float64 ??
+            
             trig_value = row['Trigger']
             GPIO_trigOn = get_GPIO_bool(trig_value, self.parralel_GPIO)
             isi = round(row['ISI'] * 10**-3, 3)
-            logger.info('Reading {}'.format(row['Stimulus']))
+            print('Reading {}'.format(row['Stimulus']))
 
-            try:
-                self.stream.start()
-                GPIO.output(GPIO_trigOn,1)
-                self.stream.write(sound_data)
-                self.stream.stop()
-            except sd.PortAudioError:
-                logger.warning('catch Exception :')
-                logger.warning(sd.PortAudioError)
-                return
-            except :
-                return
+            # get sound and ISI durations 
+            audio_duration = sound_data.shape[0] / sample_rate
+            isi_duration = round(isi, 3) 
+            
+            # stop stream
+            self.wait(stopTime, 'stop')
+            self.stream.stop()
 
+            # let previous ISI terminate
+            self.wait(endTime, 'end')
+
+            # Check accuracy
+            lastEffectiveDuration = perf_counter() - startTime
+            lastWantedDuration = endTime-startTime
+
+            # Start next sample timing
+            startTime = perf_counter()
+            audioTime = startTime + audio_duration      # end of audio in sec: time to turn off GPIO
+            stopTime = audioTime + isi_duration - 0.001 # end of audio+ISI-10ms: stop audio stream
+            endTime = audioTime + isi_duration          # end of audio+ISI: start next sample
+
+            self.stream.start()
+            GPIO.output(GPIO_trigOn,1)
+            self.stream.write(sound_data)
+
+            # Wait for audio duration : stream.write() might return before audio buffer is completely flushed
+            self.wait(audioTime, 'audio')
             GPIO.output(GPIO_trigOn, 0)
-            logger.info('isi : ', isi)
-            time.sleep(isi)
+
+            # Print accuracy
+            #print('Cycle Accuracy: ')
+            #print('\tLast cycle duration (real/target): ', round(lastEffectiveDuration, 5), '/', round(lastWantedDuration,5), 's')
+            #print('\tError:', round(lastEffectiveDuration-lastWantedDuration, 5), 's' )
+
 
         GPIO.output(self.LEDState_GPIO[4], GPIO.HIGH)
 
@@ -111,11 +136,12 @@ class sound_trig_Thread(Thread):
         self.stream.abort()
 
 
-#TODO v0.3 : LOG + state graph
+#TODO LOG
+#TODO graph d'etat
 class PyAudio_protocol_rpi():
 
     config_GPIO = { 'mode':0,  #0 BOARD 1 BCM
-            'parralel':np.array([32,18,36,37,16,33,23,21], dtype=np.int32), #Correspondance port // [9,8,7,...,2]
+            'parralel':np.array([32,18,36,37,16,33,23,21], dtype=np.int32), #Attention a l'ordre : correspondance port // : [9,8,7,6,..,2]
             #'parralel':np.array([29,31,33,35,37,36,38,40], dtype=np.int32) #basic rpi
             'butStart':7,
             'butStop':11,
@@ -129,7 +155,7 @@ class PyAudio_protocol_rpi():
         self._running = False
         self._playing = False
         self.state = 'Init'
-        logger.info('self.state : ', self.state)
+        print('self.state : ', self.state)
 
     def set_config(self, playframe, num_device=5, stim_folder='', sample_rate=44100,
             channels=2, sound_dtype='float32'):
@@ -148,6 +174,13 @@ class PyAudio_protocol_rpi():
             GPIO.setmode(GPIO.BOARD)
         else:
             GPIO.setmode(GPIO.BCM)
+        #GPIO.setwarnings(False)
+
+        #self.parralel_GPIO = self.config_GPIO['parralel']
+        #self.butStart_GPIO = self.config_GPIO['butStart']
+        #self.butStop_GPIO = self.config_GPIO['butStop']
+        #self.LEDStart_GPIO = self.config_GPIO['LED_Start']
+        #self.LEDState_GPIO = self.config_GPIO['LED_State']
 
         self.sound_trig_Thread.set_params(self.playframe,
             self.stream, self.stim_folder, self.sound_dtype, self.config_GPIO)
@@ -158,31 +191,35 @@ class PyAudio_protocol_rpi():
         [GPIO.setup(ii,GPIO.OUT) for ii in self.config_GPIO['LED_State']]
 
         self.state = 'Config'
-        logger.info('self.state : ', self.state)
+        print('self.state : ', self.state)
 
     def running(self):
         return self._running
 
     def playing(self):
-        return self._playing #could be a mutex ?
+        return self._playing #do we need a mutex ?
 
     def onStartButton(self, numGPIO):
-        logger.debug('press start')
+        print('press start')
         if not self.playing():
-            logger.debug('started')
-            self.sound_trig_Thread.start()
-            self._playing = True
+            when_pressed = time.time()
+            while GPIO.input(self.config_GPIO['butStart']):
+                time.sleep(0.001)
+                time_pressed = time.time() - when_pressed
+                if time_pressed > 1:
+                    self.sound_trig_Thread.start()
+                    self._playing = True
 
 
     def onStopButton(self, numGPIO):
-        logger.debug('Stopped')
+        print('Stopped')
         when_pressed = time.time()
         while GPIO.input(self.config_GPIO['butStop']):
             time.sleep(0.001)
-            pass
-        time_pressed = time.time() - when_pressed
-        if time_pressed > 2:
-            self.stop()
+            time_pressed = time.time() - when_pressed
+            if time_pressed > 2:
+               self.stop()
+               return
 
 
     def start(self):
@@ -193,20 +230,20 @@ class PyAudio_protocol_rpi():
 
         self.state = 'Running : stim %i %s'.format('trucTODO')
         self._running = True
-        logger.debug(self.state)
+        print(self.state)
         GPIO.output(self.config_GPIO['LED_Start'],GPIO.HIGH)
         while self.running():
             time.sleep(0.5)
 
     def pause(self):
-        #TODO v0.3
+        #TODO
         pass
 
     def stop(self):
         '''
         In this case, we want stop button stops all the process and shutdown the rpi
         '''
-        logger.debug('Stopping')
+        print('Stopping')
         GPIO.remove_event_detect(self.config_GPIO['butStart'])
         GPIO.remove_event_detect(self.config_GPIO['butStop'])
         GPIO.output(self.config_GPIO['LED_Start'],GPIO.LOW)
@@ -219,35 +256,37 @@ class PyAudio_protocol_rpi():
 
         self.stream.close()
 
-        logger.info('everything is closed, could shutdown rpi')
-        #switch off the rpi
+        print('everything is closed, could shutdown rpi')
+        #switch off the rpi - usb key is automatically unmount at shutdown
         call("sudo shutdown -h now", shell=True)
 
     def get_state(self):
         return self.state
 
     def save_results(self):
-        #TODO v0.3
+        #TODO
         pass
 
 
 
-def test_audioproto():
+def test_PyAudio_protocol_rpi():
         '''
         Test with playframe and stims given in examples folder.
         Should be run from core_rpi_nogui.py folder
         '''
         import pandas as pd
 
-        proto = PyAudio_protocol()
+        proto = PyAudio_protocol_rpi()
 
-        num_device = 5  # HifiBerry device
-        playframe_csv = './../examples/playframe_ex1.csv'
+        num_device = 5
+        playframe_csv = './../examples/playframe_test_timming.csv'
         playframe = pd.read_csv(playframe_csv)
         stim_folder = './../examples/stims_ex/'
         sample_rate = 44100
         channels = 2
         sound_dtype='float32'
+
+        print('reading playframe : ', playframe_csv)
 
         proto.set_config(playframe, num_device, stim_folder, sample_rate, channels,
             sound_dtype)
@@ -257,4 +296,4 @@ def test_audioproto():
 
 if __name__ == '__main__':
 
-    test_audioproto()
+    test_PyAudio_protocol_rpi()
